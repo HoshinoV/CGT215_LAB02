df03fcab77333348838dba152830d028cb8abe4f
Tengfei (Victor) Ma <ma865@purdue.edu>
2025-01-27 15:00:29 -0500

    Code for CGT 215 Lab2

--- CGT215_LAB02/CGT215_LAB02.cpp
+++ CGT215_LAB02/CGT215_LAB02.cpp
@@ -8,12 +8,25 @@ int main()
 	float B;
 	float X;
 
+	cout << "Hello! My name is Tengfei (Victor) Ma. Let's solve the equation: " << endl;
+	cout << "Ax + B = 0" << endl;
+	cout << "For x" << endl;
+
+	cout << endl;
+
 	cout << "Please enter a value for A: ";
 	cin >> A;
 
-	cout << "A=" << A << endl;
+	cout << "Please enter a value for B: ";
+	cin >> B;
 
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+	cout << endl;
+	cout << "Solving " << A << "x+" << B << "=0 for x..." << endl;
+	cout << endl;
+
+	X = -B / A;
+
+	cout << "The answer is:" << endl;
+	cout << "x=" << X;
 
 }
